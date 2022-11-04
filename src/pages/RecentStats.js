bab700ca2e728c5f276027c582476b0f941a19ea
ShannonTao <shannon.cxq@gmail.com>
2022-11-04 15:28:05 -0400

    results x of y and filter results by room

--- src/pages/RecentStats.js
+++ src/pages/RecentStats.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import ReactPaginate from "react-paginate";
+import { roomNames } from "../components/roomNames";
 
 const RecentStats = ({ recentData, getRoomStats, fetching }) => {
   const [postPerPage, setPostPerPage] = useState(10);
@@ -26,14 +27,18 @@ const RecentStats = ({ recentData, getRoomStats, fetching }) => {
       );
     });
 
-  console.log(recentData);
-  console.log(displayStats);
-
-  // issue is that userpost is not being set at the time of loading page.
-
   const handleSelect = (e) => {
     setPostPerPage(parseInt(e.target.value));
+    setPageNumber(0);
     getRoomStats();
+  };
+
+  const handleFilter = (e) => {
+    console.log(recentData);
+    setUserPosts(recentData.slice(0, recentData.length).reverse());
+    console.log(userPosts);
+    setUserPosts(recentData.filter((post) => post.name === e.target.value));
+    console.log(userPosts);
   };
 
   const pageCount = Math.ceil(userPosts.length / postPerPage);
@@ -42,25 +47,31 @@ const RecentStats = ({ recentData, getRoomStats, fetching }) => {
     setPageNumber(selected);
   };
 
+  // need to create a function that reads the selectoption
+  // goes into recent data array and only returns array of that e.target.value
+  // might need to manipulate user Posts or filter user Posts to return that value
+
   return (
     <section id="recentStats" className="recent-stats">
       <div className="wrapper">
         <h2>Recent Stats</h2>
         <div className="filter-posts">
           <select
-            name=""
-            id=""
-            onChange={(e) => {
-              handleSelect(e);
-            }}
-            defaultValue={"Stats Per Page"}
+            name="filter-post-select"
+            defaultValue={"Room Name"}
+            onChange={(e) => handleFilter(e)}
           >
-            <option value="Stats Per Page" disabled>
-              Stats Per Page
+            <option value="Room Name" disabled>
+              Room Name
             </option>
-            <option value="5">5</option>
-            <option value="10">10</option>
-            <option value="15">15</option>
+            <option value="All">All</option>
+            {roomNames.map((room, index) => {
+              return (
+                <option value={room.name} key={index}>
+                  {room.name}
+                </option>
+              );
+            })}
           </select>
         </div>
         <table className="room-stats-display">
@@ -76,18 +87,43 @@ const RecentStats = ({ recentData, getRoomStats, fetching }) => {
           </thead>
           <tbody>{!userPosts ? "Loading..." : displayStats}</tbody>
         </table>
+        <div className="sort-results">
+          <p>
+            Results&nbsp;
+            {pagesVisited + postPerPage > recentData.length
+              ? recentData.length
+              : `${pagesVisited + postPerPage}`}
+            &nbsp;of
+            {` ${recentData.length}`}
+          </p>
+          <select
+            name="stat-per-page"
+            id="stat-per-page"
+            onChange={(e) => {
+              handleSelect(e);
+            }}
+            defaultValue={"Stats Per Page"}
+          >
+            <option value="Stats Per Page" disabled>
+              Stats Per Page
+            </option>
+            <option value="5">5</option>
+            <option value="10">10</option>
+            <option value="15">15</option>
+          </select>
+          <ReactPaginate
+            previousLabel={"Previous"}
+            nextLabel={"Next"}
+            pageCount={pageCount}
+            onPageChange={changePage}
+            containerClassName={"pagination-btn"}
+            previousLinkClassName={"previous-btn"}
+            nextLinkClassName={"next-btn"}
+            disabledClassName={"pagination-disabled"}
+            activeClassName={"pagination-active"}
+          />
+        </div>
       </div>
-      <ReactPaginate
-        previousLabel={"Previous"}
-        nextLabel={"Next"}
-        pageCount={pageCount}
-        onPageChange={changePage}
-        containerClassName={"pagination-btn"}
-        previousLinkClassName={"previous-btn"}
-        nextLinkClassName={"next-btn"}
-        disabledClassName={"pagination-disabled"}
-        activeClassName={"pagination-active"}
-      />
     </section>
   );
 };
